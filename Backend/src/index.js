0a6091073ad9034206bbdf9c8ae4ec53aab57dba
Kavya-Work-Bit <kavya.lakdawala20@gmail.com>
2025-08-27 22:36:25 +0530

    created message endpoints

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,12 +3,14 @@ const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js"
 import DBConnection from "./lib/db.js";
 import cookieParser from "cookie-parser"
 
 app.use(express.json());
 app.use(cookieParser())
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 
 app.listen(process.env.PORT, () => {
